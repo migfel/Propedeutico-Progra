c08e2b24a9c797ba885d44163a6add578f8c1a63
Miguel Felix <migfel@users.noreply.github.com>
2023-05-18 13:52:27 -0600

    Add files via upload

---
+++ Pilas python y Java/Ventana.py
@@ -0,0 +1,11 @@
+class Ventana:
+    def cerrar(self):
+        print("Ventana cerrada.")
+
+    def minimizar(self):
+        print("Ventana minimizada.")
+
+
+ventana = Ventana()
+ventana.cerrar()
+ventana.minimizar()
